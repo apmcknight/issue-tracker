721301fb006ce4fe797661253fad8a9593304b79
Adam McKnight <adammcknight@me.com>
2021-02-08 14:37:29 -0500

    First Commit

---
+++ Models/Issues.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace issue_tracker.Models
+{
+    public class Issues
+    {
+        public Issues()
+        {
+        }
+    }
+}
---
+++ Services/IssueService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace issue_tracker.Services
+{
+    public class IssueService
+    {
+        public IssueService()
+        {
+        }
+    }
+}
